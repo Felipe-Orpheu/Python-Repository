6abc9b54441c1e2cba588637f36ec249a5d5e68d
Felipe-Orpheu <111926105+Felipe-Orpheu@users.noreply.github.com>
2023-11-13 18:25:20 -0300

    Add files via upload

---
+++ Curso Python/Modulo 05/01 tuplas.py
@@ -0,0 +1,12 @@
+lista = ["item 1", "item 2", "item 3"] #Exemplos de tupla
+print(lista)
+print(len(lista))
+print(type(lista))
+
+print("------"*10)
+
+tuplas = ("item 1", "item 2", "item 3", "item 1", "item 1") #Exemplos de lista 
+print(tuplas)
+print(len(tuplas))
+print(type(tuplas))
+print(tuplas.count("item 1"))
