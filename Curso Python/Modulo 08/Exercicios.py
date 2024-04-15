d5e99c335fd740f7e7f00677cd09e389e2999e4b
Felipe-Orpheu <111926105+Felipe-Orpheu@users.noreply.github.com>
2024-04-15 16:37:24 -0300

    Update Exercicios.py

--- Curso Python/Modulo 08/Exercicios.py
+++ Curso Python/Modulo 08/Exercicios.py
@@ -29,3 +29,25 @@ def num_return():
 print(f"O resultado de {n_x} + {n_y} é igual a {num_return()}")
 """
 
+# Calculadora 
+
+print("-"*30, "Calculadora", "-"*30)
+
+operacao = input("Por favor escolha o que ira fazer (+, -, *, /): ")
+n1 = int(input("Digite um numero para calcular: "))
+n2 = int(input("Digite outro numero para calcular: "))
+
+if operacao == '+': 
+    print(f'{n1} + {n2}')
+    print(n1 + n2)
+elif operacao == '-': 
+    print(f'{n1} - {n2}')
+    print(n1 - n2)
+elif operacao == '*': 
+    print(f'{n1} * {n2}')
+    print(n1 * n2)
+elif operacao == '/': 
+    print(f'{n1} / {n2}')
+    print(n1 / n2)
+else: 
+    print("Oh seu animal, como q tu deu erro em uma calculadora, explica pra mim?")
